14787580551deb61b145cb308a24ece0f4d04881
Austin <pachano.austin@gmail.com>
2018-01-04 12:29:01 -0800

    cleaned up code

--- init.lua
+++ init.lua
@@ -1,19 +1,14 @@
-ore = {}
-
-ore.name = "iron"
-ore.resources = "/resources"
-ore.spawn = {
-	ore_type       = "scatter",
-	wherein        = "default:stone",
-	clust_scarcity = 7*7*7,
-	clust_num_ores = 5,
-	clust_size     = 3,
-	height_min     = -31000,
-	height_max     = -4000,
-	flags          = "absheight",
-}
-
-ore.tools = {}
-ore.extended = {}
-
-advancedores_core.add_ore(ore)
+advancedores_add_ore({
+	name = "iron",
+	resources = "/resources",
+	spawn = {
+		ore_type       = "scatter",
+		wherein        = "default:stone",
+		clust_scarcity = 7*7*7,
+		clust_num_ores = 5,
+		clust_size     = 3,
+		height_min     = -31000,
+		height_max     = -4000,
+		flags          = "absheight",
+	}
+})
